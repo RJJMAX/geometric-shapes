b8530847f02686421de95858e4c46838934de34e
Rob Jaindl <robmax2023@outlook.com>
2023-02-15 11:54:26 -0500

    Add project files.

---
+++ geometric-shapes/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using geometric_shapes;
+
+sqr s1 = new sqr(5);
+
+Console.WriteLine($"Perimeter is {s1.Perimeter()} and the Area is {s1.Area()}");
+
+
+rect r1 = new rect(3, 5);
+
+
+Console.WriteLine($"Perimeter is {r1.Perimeter()} and Area is {r1.Area()}.");
+
+
+quad q1 = new quad(3, 4, 5, 6); //side1, side2, side3, side4
+
+
+Console.WriteLine($"Perimeter is {q1.Perimeter()}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Console.WriteLine($"2 plus 3 equals {MathLib.Add(2, 3)}");
+//Console.WriteLine($"2 minus 3 equals {MathLib.Subtract(2, 3)}");
+//Console.WriteLine($"2 times 3 equals {MathLib.Multiply(2, 3)}");
+//Console.WriteLine($"2 divided by 3 equals {MathLib.Divide(2, 0)}");
+
+
+//var fullname = Person.Fullname("Rob", "Jaindl");
+
+//Console.WriteLine(fullname);
+
---
+++ geometric-shapes/sqr.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace geometric_shapes; 
+internal class sqr {
+
+    public int Side1 { get; set; }
+
+    public int Perimeter() {
+        return Side1 * 4;
+        return Side1 * Side1;
+
+        }
+    public int Area() {
+        return Side1 * Side1;
+    }
+    public sqr() { }
+
+    public sqr(int side1) {
+        Side1 = side1;
+    }
+}
